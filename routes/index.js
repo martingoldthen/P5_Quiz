dc4b287d391af556a654fbf0837c318026132d40
martingoldthen <martingon03@gmail.com>
2018-05-10 12:55:03 +0200

    version 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const Sequelize = require('sequelize');
+const sequelize = require("../models/index");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'P5_Quiz' });
+});
+
+/* GET credits */
+router.get('/credits', function(req, res, next) {
+    res.render('credits', { title:'Quiz', name: 'Martin Gonzalez Calvo' });
+});
+
+router.get('/quizzes', function(req, res, next) {
+    sequelize.models.quiz.findAll().
+    then( quizzes => {
+        res.render('quizzes', {quizzes});
+})
+.catch(Sequelize.ValidationError, error => {
+    })
+.catch(error => {
+    });
 });
 
 module.exports = router;
